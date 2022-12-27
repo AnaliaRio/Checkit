71247ea1a850c45e10ac7573f7a7e8da6682dcdd
Analía Río <analia.rio@gmail.com>
2022-12-27 20:26:02 +0100

    Changed Section 4

--- src/components/Section4.js
+++ src/components/Section4.js
@@ -3,22 +3,15 @@ import Collab from "../images/collab.jpg";
 
 const Section4 = () => {
   return (
-    <section className='section1' id='linkToAbout'>
-      <h2 className='section1__title'>colabora con nosotros</h2>
+    <section className='section4' id='linkToAbout'>
+      <h2 className='section4__title'>colabora con nosotros</h2>
       <div className='div'>
         <img className='imgB' src={Collab} alt='' />
-        <div>
-          <p className='section1__boxparagraph1'>
-            Si estás satisfecho con nuestro trabajo, agradeceríamos un aporte
-            solidario para ayudarnos a costear el mantenimiento de nuestra web.
-          </p>
-          <p className='section1__boxparagraph2'>
-            Texto texto texto texto texto texto.
-          </p>
-          <p className='section1__boxparagraph2'>
-            Texto texto texto texto texto.
-          </p>
-        </div>
+        <p className='section4__boxparagraph1'>
+          Gracias por ayudarnos y creer en nuestra causa, tus fondos se
+          utilizarán para permitirnos continuar asesorando gratuitamente a todas
+          las empresas que lo necesitan.
+        </p>
       </div>
     </section>
   );
