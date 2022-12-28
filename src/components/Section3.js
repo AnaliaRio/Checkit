2b279ccc5a3d7c7cad4434b64820b574ca6b29b2
acatoro <37416076+acatoro@users.noreply.github.com>
2022-12-28 10:42:30 +0100

    Update Section3.js

--- src/components/Section3.js
+++ src/components/Section3.js
@@ -34,7 +34,7 @@ const Section3 = (props) => {
           <h3 className='section3__boxtitle'>Mauro Piano</h3>
           <h3 className='section3__boxsubtitle'>Analista de datos</h3>
           <p className='section3__boxparagraph'>
-            Analista de datos con Certificacion Profesional de Análisis de Datos
+            Reconocido con Certificacion Profesional de Análisis de Datos
             de Google.
           </p>
         </div>
