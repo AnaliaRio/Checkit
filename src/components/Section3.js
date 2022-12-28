f55b5cb574e002a81cd3dc798c78280aab7cff06
Analía Río <analia.rio@gmail.com>
2022-12-28 16:31:10 +0100

    Added Linkedin links

--- src/components/Section3.js
+++ src/components/Section3.js
@@ -1,4 +1,6 @@
 import "../styles/components/Section3.scss";
+import { FaLinkedin } from "react-icons/fa";
+
 import Analia from "../images/Analia.jpg";
 import Felipe from "../images/Felipe.jpg";
 import Jose from "../images/Jose.jpg";
@@ -13,21 +15,39 @@ const Section3 = (props) => {
       <div className='section3__boxes grid'>
         <div class='section3__box'>
           <img className='pic' src={Jose} alt='' />
-          <h3 className='section3__boxtitle'>José Luis Rodríguez</h3>
-          <h3 className='section3__boxsubtitle'>Organizador del proyecto</h3>
-          <p className='section3__boxparagraph'>Texto sobre José.</p>
+          <h3 className='section3__boxtitle'>José Luis Rodríguez Palma</h3>
+          <h3 className='section3__boxsubtitle'>
+            Organizador - Diseño Web y Green IT
+          </h3>
+          <a
+            href='https://www.linkedin.com/in/jose-rodriguez-65961a256/'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <FaLinkedin className='fa' />
+          </a>
         </div>
         <div class='section3__box'>
           <img className='pic' src={Esperanza} alt='' />
           <h3 className='section3__boxtitle'>Esperanza Castillo</h3>
-          <h3 className='section3__boxsubtitle'>Experta en marketing</h3>
+          <h3 className='section3__boxsubtitle'>Marketing</h3>
           <p className='section3__boxparagraph'>Texto sobre Esperanza.</p>
+          <a
+            href='https://www.linkedin.com/in/esperanza-eco/'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <FaLinkedin className='fa' />
+          </a>
         </div>
         <div class='section3__box'>
           <img className='pic' src={Osiris} alt='' />
           <h3 className='section3__boxtitle'>Osiris Herrera</h3>
-          <h3 className='section3__boxsubtitle'>Experto en ciberseguridad</h3>
+          <h3 className='section3__boxsubtitle'>Ciberseguridad</h3>
           <p className='section3__boxparagraph'>Texto sobre Osiris.</p>
+          {/* <a href='#' target='_blank' rel='noreferrer'>
+            <FaLinkedin className='fa' />
+          </a> */}
         </div>
         <div class='section3__box'>
           <img className='pic' src={Mauro} alt='' />
@@ -37,12 +57,22 @@ const Section3 = (props) => {
             Analista de datos con Certificacion Profesional de Análisis de Datos
             de Google.
           </p>
+          {/* <a href='#' target='_blank' rel='noreferrer'>
+              <FaLinkedin className='fa' />
+          </a> */}
         </div>
         <div class='section3__box'>
           <img className='pic' src={Felipe} alt='' />
           <h3 className='section3__boxtitle'>Felipe Martínez</h3>
-          <h3 className='section3__boxsubtitle'>Experto en ciberseguridad</h3>
+          <h3 className='section3__boxsubtitle'>Chief Technology Officer</h3>
           <p className='section3__boxparagraph'>Texto sobre Felipe.</p>
+          <a
+            href='https://www.linkedin.com/in/felipe-martinez-gonzalez/'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <FaLinkedin className='fa' />
+          </a>
         </div>
         <div class='section3__box'>
           <img className='pic' src={Analia} alt='' />
@@ -52,6 +82,13 @@ const Section3 = (props) => {
             Programadora con más de un año de experiencia en diferentes
             tecnologías y profesorado de idiomas.
           </p>
+          <a
+            href='https://www.linkedin.com/in/analia-rio/'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <FaLinkedin className='fa' />
+          </a>
         </div>
       </div>
     </section>
